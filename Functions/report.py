b90298104051108cec2066576fccdc8080860ac6
Stephen Enke <stephenenke@gmail.com>
2020-11-29 12:53:45 -0600

    added cross-validated

--- Functions/report.py
+++ Functions/report.py
@@ -32,28 +32,16 @@ def report(model, ind_train, ind_test, save_as = None):
     for variable in high_coefs:
         print('Variable: {}\nCoefficient: {}\n'
               .format(variable[1],variable[0]))
-    print('************')
-    print('************\nLow Impact Variables:\n')
-    for variable in low_coefs:
-        print('Variable: {}\nCoefficient: {}\n'
-              .format(variable[1],variable[0]))
-    print('************')
+ 
 
-    #Print MSE for the train an test set.
-    train_mse = mse(y_train, pred_y_train)
-    test_mse = mse(y_test, pred_y_test)
-    print('\nTrain MSE: {}\nTest MSE: {}\nDifference:{}\n'
-          .format((train_mse),(test_mse),
-                  (train_mse-test_mse)))
-    
     #Print R^2 against the test data. 
     print('Train R^2: {}'
           .format((r2_score(y_train,pred_y_train))))
-#     print('CrossValidated R^2: {}'
-#           .format(np.mean(cross_val_score(model,
-#                                           ind_train, y_train, 
-#                                           scoring ='r2', 
-#                                           cv = crossval))))
+    print('CrossValidated R^2: {}'
+          .format(np.mean(cross_val_score(model,
+                                          ind_train, y_train, 
+                                          scoring ='r2', 
+                                          cv = crossval))))
     print('Test R^2: {}\n'
           .format(r2_score(y_test,pred_y_test)))
     
@@ -62,12 +50,10 @@ def report(model, ind_train, ind_test, save_as = None):
     plotdf['test_resids'] = pred_y_test-y_test
     plotdf['y_test'] = y_test
     
-    sns.jointplot(x = 'y_test', y = 'test_resids',
-                  data=plotdf, kind='kde')
-    plt.show();
+
     
-    plt.scatter(pred_y_test, y_test, alpha=.15)
-    plt.plot(pred_y_test, pred_y_test, color='black', 
+    plt.scatter(pred_y_test, y_test, alpha=.10, c='deepskyblue')
+    plt.plot(pred_y_test, pred_y_test, c='gold', 
              label='Predicted Price')
     plt.xlabel('Predicted Price')
     plt.ylabel('Actual Price')
